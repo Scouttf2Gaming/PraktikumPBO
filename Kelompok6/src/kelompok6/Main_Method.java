78778899b36f13a01c3d2900716915e6b6ee4b62
BABIBUBAB <115200515+Scouttf2Gaming@users.noreply.github.com>
2022-10-27 12:36:32 +0700

    Add files via upload

---
+++ Kelompok6/src/kelompok6/Main_Method.java
@@ -0,0 +1,15 @@
+package kelompok6;
+/**
+ *
+ * @author Kelompok 6
+ */
+public class Main_Method {
+    public static void main(String[] args) {
+        Syntax_TebakAngka p1 = new Syntax_TebakAngka("", 0, 0, 100);
+        p1.getPemain();
+        p1.getLevel();
+        p1.tampil();
+        
+    }
+    
+}
---
+++ Kelompok6/src/kelompok6/Syntax_TebakAngka.java
@@ -0,0 +1,197 @@
+package kelompok6;
+;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Kelompok 6
+ * David giri K
+ * Hafiz Pratama
+ * Keysha Angelia P
+ * Akbar Wahyu P
+ * Muhammad Ardhan Riznal
+ * Muhammad Rayhan R
+ * 
+ */
+public class Syntax_TebakAngka {
+
+    private String pemain;
+    int jawab;
+    private int level;
+    private int nilai = (int) (100);
+    int a = (int) (Math.random() * 50) + 50;;
+    int b = (int) (Math.random() * 30) + 20;
+    int c = (int) (Math.random() * 10) + 1;
+
+    public String getPemain() {
+        return pemain;
+    }
+
+    public void setPemain(String pemain) {
+        this.pemain = pemain;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getNilai() {
+        return nilai;
+    }
+
+    public void setNilai(int nilai) {
+        this.nilai = nilai;
+    }
+
+    Syntax_TebakAngka(String pemain, int level, int jawab, int nilai) {
+        this.pemain = pemain;
+        this.level = level;
+        this.jawab = jawab;
+        this.nilai = nilai;
+
+    }
+
+    private void nama() {
+        JOptionPane.showMessageDialog(null,"SELAMAT DATANG DI PERMAINAN TEBAK ANGKA KELOMPOK 6","HOMEPAGE",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null,"BERIKUT ADALAH CARANNYA"+"\n>> Setiap pemain akan diberi skor awal 100 ,ika jawaban benar +10 dan jika jawaban salah -5 <<"+
+        "\n1.Pertama masukan USERNAME"+
+        "\n2.Kedua pilih tingkatan kesulitan"+
+        "\n-ADVANCE(Soal Acak dari 50-100)"+
+        "\n-MEDIUM(Soal Acak dari 20-50)"+
+        "\n-EASY(Soal Acak dari 0-10)"+
+        "\n3.Ketiga masukan jawaban pada kolom jawaban"+
+        "\n4.Keempat ulangi sampai mendapatkan nilai Jawaban yg benar");
+        pemain = JOptionPane.showInputDialog(null, "MASUKAN USERNAME:", "NAMA PEMAIN", JOptionPane.INFORMATION_MESSAGE);
+
+    }
+
+    private void kesulitan() {
+        this.nama();
+        String[] options = { "ADVANCE", "MEDIUM", "EASY" };
+        level = JOptionPane.showOptionDialog(null, "PILIH TINGKAT KESULITAN ", "DIFFICULTY", JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+
+    }
+
+    void tampil() {
+        this.kesulitan();
+        JOptionPane.showMessageDialog(null, "SCORE AWAL     : " + this.nilai, "NILAI AWAL",
+                JOptionPane.INFORMATION_MESSAGE);
+        do {
+            if (level == 0) {
+                JOptionPane.showMessageDialog(null, "TEBAK ANGKA DARI 50-100", "JARAK ANGKA",
+                        JOptionPane.INFORMATION_MESSAGE);
+                jawab = Integer.parseInt(JOptionPane.showInputDialog(null, "MASUKKAN ANGKA YANG ANDA TEBAK????",
+                        "JAWAB", JOptionPane.INFORMATION_MESSAGE));
+
+                if (jawab > 100 || jawab < 50) {
+                    JOptionPane.showMessageDialog(null, "yang anda masukan tidak valid", " ",
+                            JOptionPane.INFORMATION_MESSAGE);
+                } else if (jawab == a) {
+                    this.nilai += 10;
+                    JOptionPane.showMessageDialog(null,
+                            "CONGRATULATION!!! Angka yang anda masukkan BENAR!", " ", JOptionPane.INFORMATION_MESSAGE);
+
+                } else if (jawab < a) {
+                    this.nilai -= 5;
+                    JOptionPane.showMessageDialog(null,
+                            "Maaf , angka yang anda masukkan terlalu kecil" + "\nskor anda berkurang 5", " ",
+                            JOptionPane.INFORMATION_MESSAGE);
+                } else if (jawab > a) {
+                    this.nilai -= 5;
+                    JOptionPane.showMessageDialog(null,
+                            "Maaf , angka yang anda masukkan terlalu besar" + "\nskor anda berkurang 5", " ",
+                            JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                }
+            } else if (level == 1) {
+                JOptionPane.showMessageDialog(null, "TEBAK ANGKA DARI 20-50", "JARAK ANGKA",
+                        JOptionPane.INFORMATION_MESSAGE);
+                jawab = Integer.parseInt(JOptionPane.showInputDialog(null, "MASUKKAN ANGKA YANG ANDA TEBAK????",
+                        "JAWAB", JOptionPane.INFORMATION_MESSAGE));
+
+                if (jawab > 50 || jawab < 20) {
+                    JOptionPane.showMessageDialog(null, "yang anda masukan tidak valid", " ",
+                            JOptionPane.INFORMATION_MESSAGE);
+                } else if (jawab == b) {
+                    this.nilai += 10;
+                    JOptionPane.showMessageDialog(null,
+                            "CONGRATULATION!!! Angka yang anda masukkan BENAR!", " ", JOptionPane.INFORMATION_MESSAGE);
+                } else if (jawab < b) {
+                    this.nilai -= 5;
+                    JOptionPane.showMessageDialog(null,
+                            "Maaf , angka yang anda masukkan terlalu kecil" + "\nskor anda berkurang 5", " ",
+                            JOptionPane.INFORMATION_MESSAGE);
+                } else if (jawab > b) {
+                    this.nilai -= 5;
+                    JOptionPane.showMessageDialog(null,
+                            "Maaf , angka yang anda masukkan terlalu besar" + "\nskor anda berkurang 5", " ",
+                            JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                }
+            } else if (level == 2) {
+                JOptionPane.showMessageDialog(null, "TEBAK ANGKA DARI 0-10", "JARAK ANGKA",
+                        JOptionPane.INFORMATION_MESSAGE);
+                jawab = Integer.parseInt(JOptionPane.showInputDialog(null, "MASUKKAN ANGKA YANG ANDA TEBAK????",
+                        "JAWAB", JOptionPane.INFORMATION_MESSAGE));
+
+                if (jawab > 10 || jawab < 0) {
+                    JOptionPane.showMessageDialog(null, "yang anda masukan tidak valid", " ",
+                            JOptionPane.INFORMATION_MESSAGE);
+                } else if (jawab == c) {
+                    this.nilai += 10;
+                    JOptionPane.showMessageDialog(null,
+                            "CONGRATULATION!!! Angka yang anda masukkan BENAR!", " ", JOptionPane.INFORMATION_MESSAGE);
+                } else if (jawab < c) {
+                    this.nilai -= 5;
+                    JOptionPane.showMessageDialog(null,
+                            "Maaf , angka yang anda masukkan terlalu kecil" + "\nskor anda berkurang 5", " ",
+                            JOptionPane.INFORMATION_MESSAGE);
+                } else if (jawab > c) {
+                    this.nilai -= 5;
+                    JOptionPane.showMessageDialog(null,
+                            "Maaf , angka yang anda masukkan terlalu besar" + "\nskor anda berkurang 5", " ",
+                            JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                }
+            } else {
+            }
+            if (jawab == a) {
+                JOptionPane.showMessageDialog(null, "USERNAME      : " + pemain
+                        + "\nSCORE AKHIR : " + this.nilai, "RESULTS", JOptionPane.INFORMATION_MESSAGE);
+                System.exit(0);
+
+            }else if(jawab == b){
+                JOptionPane.showMessageDialog(null, "USERNAME      : " + pemain
+                        + "\nSCORE AKHIR : " + this.nilai, "RESULTS", JOptionPane.INFORMATION_MESSAGE);
+                System.exit(0);
+
+            }else if(jawab == c){
+                JOptionPane.showMessageDialog(null, "USERNAME      : " + pemain
+                        + "\nSCORE AKHIR : " + this.nilai, "RESULTS", JOptionPane.INFORMATION_MESSAGE);
+                System.exit(0);
+            }else{
+            }
+        } while (JOptionPane.showConfirmDialog(null, "MAU MENGULANG UNTUK MENEBAK ?", "Pilih Salah Satu (Y/N)",
+                JOptionPane.INFORMATION_MESSAGE) == JOptionPane.YES_OPTION);
+
+        if (level == 0) {
+            JOptionPane.showMessageDialog(null, "JAWABAN NYA ADALAH " + a, "JAWABAN", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "USERNAME      : " + pemain
+                    + "\nSCORE AKHIR : " + this.nilai, "RESULTS", JOptionPane.INFORMATION_MESSAGE);
+        } else if (level == 1) {
+            JOptionPane.showMessageDialog(null, "JAWABAN NYA ADALAH " + b, "JAWABAN", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "USERNAME      : " + pemain
+                    + "\nSCORE AKHIR : " + this.nilai, "RESULTS", JOptionPane.INFORMATION_MESSAGE);
+        } else if (level == 2) {
+            JOptionPane.showMessageDialog(null, "JAWABAN NYA ADALAH " + c, "JAWABAN", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "USERNAME      : " + pemain
+                    + "\nSCORE AKHIR : " + this.nilai, "RESULTS", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+        }
+    }
+}
